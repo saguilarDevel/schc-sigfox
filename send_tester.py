444c3a11bf7cf07f7b8937c784c093aebb144413
saguilardevel <saguilardevel@gmail.com>
2020-10-09 15:42:51 +0200

    added example of two messages that can be sent to google cloud function

--- send_tester.py
+++ send_tester.py
@@ -5,6 +5,7 @@ import time
 
 def send_request(sendData, timeout):
     url = 'http://localhost:5000/hello_get'
+    url_cloud = 'https://us-central1-schc-sigfox-upc.cloudfunctions.net/hello_get'
     headers = {'content-type': 'application/json'}
     print('sendData -> {}'.format(sendData))
     # data_dict = {
@@ -15,8 +16,8 @@ def send_request(sendData, timeout):
     #     "seqNumber": "39",
     #     "ack": "false"
     # }
-    response = requests.post(url, data=json.dumps(sendData), headers=headers, timeout=timeout)
-    print('response -> {}'.format(response))
+    response = requests.post(url_cloud, data=json.dumps(sendData), headers=headers, timeout=timeout)
+    # print('response -> {}'.format(response))
     return response
 
 
@@ -28,23 +29,38 @@ msg1 = {
   "seqNumber": "49",
   "ack": "false"
 }
-msg2 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1599043019', 'data': '053131313231333134313531',
-        'seqNumber': '50', 'ack': 'false'}
-msg3 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1599043030', 'data': '073132333435',
-        'seqNumber': '51', 'ack': 'false'}
-# msg4 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1599043040', 'data': '033232323332343235323632',
-#         'seqNumber': '52', 'ack': 'false'}
-# msg5 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1599043047', 'data': '023732383239333033313332',
-#         'seqNumber': '53', 'ack': 'false'}
-# msg6 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1599043056', 'data': '013333333433353336333733',
-#         'seqNumber': '54', 'ack': 'false'}
-# msg7 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1599043069', 'data': '003833393430343134323433',
-#         'seqNumber': '55', 'ack': 'false'}
+msg2 = {"deviceType": "01B29CC4", "device": "1B29CC4", "time": "1599043019", "data": "053131313231333134313531",
+        "seqNumber": "50", "ack": "false"}
+msg3 = {"deviceType": "01B29CC4", "device": "1B29CC4", "time": "1599043030", "data": "073132333435",
+        "seqNumber": "51", "ack": "true"}
+
+
+msg2_1 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249495', 'data': '063132333435363738393132',
+          'seqNumber': '391', 'ack': 'false'}
+msg2_2 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249504', 'data': '053334353637383931323334',
+          'seqNumber': '392', 'ack': 'false'}
+msg2_3 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249513', 'data': '043536373839313233343536',
+          'seqNumber': '393', 'ack': 'false'}
+msg2_4 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249523', 'data': '033738393132333435363738',
+          'seqNumber': '394', 'ack': 'false'}
+msg2_5 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249533', 'data': '023931323334353637383931',
+          'seqNumber': '395', 'ack': 'false'}
+msg2_6 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249545', 'data': '013233343536373839313233',
+          'seqNumber': '396', 'ack': 'false'}
+msg2_7 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249554', 'data': '003435363738393132333435',
+          'seqNumber': '397', 'ack': 'true'}
+msg2_8 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249599', 'data': '0e3637383931323334353637',
+          'seqNumber': '398', 'ack': 'false'}
+msg2_9 = {'deviceType': '01B29CC4', 'device': '1B29CC4', 'time': '1602249609', 'data': '0f3839',
+          'seqNumber': '399', 'ack': 'true'}
+
+
 
 # msgs = [msg1, msg2, msg3, msg4, msg5, msg6, msg7]
-msgs = [msg1, msg2, msg3]
+msgs1 = [msg1, msg2, msg3]
+msgs2 = [msg2_1, msg2_2, msg2_3, msg2_4, msg2_5, msg2_6, msg2_7, msg2_8, msg2_9]
 
-for i, msg in enumerate(msgs):
+for i, msg in enumerate(msgs2):
     print('sending msg {}, {}'.format(i, msg))
     response = send_request(msg, 45)
     if response.status_code == 204:
@@ -53,7 +69,7 @@ for i, msg in enumerate(msgs):
         print('Error in server')
         break
     elif response.status_code == 200:
-        print('response -> '.format(response.json()))
+        print('response -> {}'.format(response.text))
 
     time.sleep(1)
 
