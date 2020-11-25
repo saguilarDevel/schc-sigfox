6ebe1621a4fb70f924da79a3636cec7e081c0ae8
saguilardevel <saguilardevel@gmail.com>
2020-11-25 10:22:45 +0100

    added config.py to solve issue while working with different configuration of BUCKET_NAME and CLIENT_SECRET_FILE, added change in main.py to handle a enable losses flag and loss rate from the sigfox cloud message. Changes in test_server.py to work with the config.py file created.

---
+++ config/config.py
@@ -0,0 +1,11 @@
+# Configuration variables for SCHCFOX
+# Change the variables name to your files, then remove track from git
+# git rm --cached config/config.py
+
+# Cloud Storage Bucket Name
+BUCKET_NAME = 'sigfoxschc'
+#
+CLIENT_SECRETS_FILE = '/Users/sergioaguilar/PycharmProjects/SCHCfox/credentials/schc-sigfox-upc-f573cd86ed0a.json'
+
+# File where we will store authentication credentials after acquiring them.
+CREDENTIALS_FILE = '/Users/sergioaguilar/PycharmProjects/SCHCfox/credentials/schc-sigfox-upc-f573cd86ed0a.json'
--- stats/analytics.py
+++ stats/analytics.py
@@ -1,15 +1,49 @@
 import pandas as pd
 import json
 
-# json_file = json.loads('/Users/sergioaguilar/PycharmProjects/SCHCfox/stats/files/stats_file.json')
+pd.set_option('display.max_columns', None)
+
+# json_file = json.loads('/Users/sergioaguilar/PycharmProjects/SCHCfox/stats/files/stats_file_v2.json')
 with open('files/stats_file.json') as json_file:
     data = json.load(json_file)
 
-df1 = pd.read_json(str(json.dumps(data, sort_keys=True)))
+tx_json = {}
+tx_json['fragmentation_time'] = data['fragmentation_time']
+tx_json['payload_size'] = data['payload_size']
+tx_json['total_number_of_fragments'] = data['total_number_of_fragments']
+tx_json['total_transmission_time'] = data['total_transmission_time']
+print(tx_json)
+df_tx = pd.DataFrame(tx_json, index=[0])
+df_tx = df_tx.T
+print(df_tx)
+
+assert data['fragments']
+df1 = pd.read_json(str(json.dumps(data['fragments'], sort_keys=True)))
+# print(df1)
+# df1.astype({"Column 1": int, "Column 2": int, "Column 3": int})
+# df1.astype({"Column 6": bool})
+# print(df1.dtypes)
 df1_transposed = df1.T # or df1.transpose()
 print(df1_transposed)
-print(df1.dtypes)
-# df1.astype({"Column 1": int, "Column 2": int, "Column 3": int})
+df1_transposed.astype({"FCN": str, "RULE_ID": str,  "W": str,  "ack": str, "data": str, "download_enable": bool,
+                       "rssi": int, "send_time": float, "sending_end": float, "sending_start": float})
+# print(df1_transposed.dtypes)
+# print(df1_transposed.columns)
+# print(df1_transposed[df1_transposed['download_enable'].isin([False])])
+
+df_nowait = df1_transposed[df1_transposed['download_enable'].isin([False])]
+print("Fragments - no dowload requested")
+print(df_nowait['send_time'])
+print("std:{}".format(df_nowait['send_time'].std(axis=0, skipna=True)))
+print("mean:{}".format(df_nowait['send_time'].mean(axis=0, skipna=True)))
+
+df_wait = df1_transposed[df1_transposed['download_enable'].isin([True])]
+print("Fragments - dowload requested")
+print(df_wait['send_time'])
+print("std:{}".format(df_wait['send_time'].std(axis=0, skipna=True)))
+print("mean:{}".format(df_wait['send_time'].mean(axis=0, skipna=True)))
+
+
 # print(df1['FCN'])
 
 
